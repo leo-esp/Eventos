bef82255142072b519294e7778ab03a4c122ac79
Leonardo Esposito <leo_esp_5@hotmail.com>
2016-09-17 15:45:22 -0300

    aidcionando mais informacoes aos ursos

--- database/models/bear.js
+++ database/models/bear.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BearSchema = new Schema({
-    name: String
+    name: String,
+    noise: String,
+    size: Number,
+    color: String
 });
 
 module.exports = mongoose.model('Bear', BearSchema);
--- index.js
+++ index.js
@@ -4,73 +4,79 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://herogod:esposito@ds027896.mlab.com:27896/beardb'); // connect to our database
 var Bear = require('./database/models/bear');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
     console.log('Something is happening...');
     next();
 });
 
-router.get('/', function(req, res){
-    res.json({message: 'hooray! welcome to our api!' });
+router.get('/', function (req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
 router.route('/bears')
-    .post(function(req, res){
+    .post(function (req, res) {
         var bear = new Bear();
         bear.name = req.body.name;
-        bear.save(function(err){
-            if(err)
+        bear.noise = req.body.noise;
+        bear.size = req.body.size;
+        bear.color = req.body.color;
+        bear.save(function (err) {
+            if (err)
                 res.send(err);
-            
-            res.json({message: 'Bear created!'});
+
+            res.json({ message: 'Bear created!' });
         });
     })
-    .get(function(req, res){
-        Bear.find(function(err, bears){
-            if(err)
+    .get(function (req, res) {
+        Bear.find(function (err, bears) {
+            if (err)
                 res.send(err);
-            
+
             res.json(bears);
         });
     });
 
 router.route('/bears/:bear_id')
-    .get(function(req, res){
-        Bear.findById(req.params.bear_id, function(err, bear){
-            if(err)
+    .get(function (req, res) {
+        Bear.findById(req.params.bear_id, function (err, bear) {
+            if (err)
                 res.send(err);
-            
+
             res.json(bear);
         });
     })
-    .put(function(req, res){
-        Bear.findById(req.params.bear_id, function(err, bear){
-            if(err)
+    .put(function (req, res) {
+        Bear.findById(req.params.bear_id, function (err, bear) {
+            if (err)
                 res.send(err);
-            
+
             bear.name = req.body.name;
-            bear.save(function(err){
-                if(err)
+            bear.noise = req.body.noise;
+            bear.size = req.body.size;
+            bear.color = req.body.color;
+            bear.save(function (err) {
+                if (err)
                     res.send(err);
-                
-                res.json({message: 'Bear updated!'});
+
+                res.json({ message: 'Bear updated!' });
             });
         });
     })
-    .delete(function(req, res){
+    .delete(function (req, res) {
         Bear.remove({
             _id: req.params.bear_id
-        }, function(err, bear) {
-            if(err)
+        }, function (err, bear) {
+            if (err)
                 res.send(err);
-            
-            res.json({message: 'Sucessfully deleted' });
+
+            res.json({ message: 'Sucessfully deleted' });
         });
     });
 
