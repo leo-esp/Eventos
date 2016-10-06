8382a1aa69741095c532f7b57e819fcee1dc0918
Leonardo Esposito <leo_esp_5@hotmail.com>
2016-10-05 22:36:08 -0300

    .

--- index.js
+++ index.js
@@ -1,59 +1,83 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://herogod:esposito@ds027896.mlab.com:27896/beardb'); // connect to our database
 var Bear = require('./database/models/bear');
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(expressValidator());
 
 var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
-router.use(function (req, res, next) {
+router.use(function(req, res, next) {
     console.log('Something is happening...');
     next();
 });
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
 router.route('/bears')
-    .post(function (req, res) {
+    .post(function(req, res) {
         var bear = new Bear();
         bear.name = req.body.name;
         bear.noise = req.body.noise;
         bear.size = req.body.size;
         bear.color = req.body.color;
-        bear.save(function (err) {
+
+        req.assert('name', 'Nome é obrigatório').notEmpty();
+        req.assert('size', 'O tamanho é obrigatório').notEmpty();
+        req.assert('size', 'O tamanho precisa ser um número').isFloat();
+        req.assert('color', 'A cor é obrigatória').notEmpty();
+
+        var validatorErrors = req.validationErrors();
+        if (validatorErrors) {
+            res.format({
+                json: function() {
+                    res.status(400).json(validatorErrors);
+                }
+            });
+        }
+
+        bear.save(function(err) {
             if (err)
                 res.send(err);
 
             res.json({ message: 'Bear created!' });
         });
     })
-    .get(function (req, res) {
-        Bear.find(function (err, bears) {
+    .get(function(req, res) {
+        Bear.find(function(err, bears) {
             if (err)
                 res.send(err);
-
-            res.json(bears);
+            res.format({
+                html: function() {
+                    res.render('bears', { data: bears });
+                },
+                json: function() {
+                    res.json(bears);
+                }
+            });
         });
     });
 
 router.route('/bears/:bear_id')
-    .get(function (req, res) {
-        Bear.findById(req.params.bear_id, function (err, bear) {
+    .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
             if (err)
                 res.send(err);
 
             res.json(bear);
         });
     })
-    .put(function (req, res) {
-        Bear.findById(req.params.bear_id, function (err, bear) {
+    .put(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
             if (err)
                 res.send(err);
 
@@ -61,7 +85,7 @@ router.route('/bears/:bear_id')
             bear.noise = req.body.noise;
             bear.size = req.body.size;
             bear.color = req.body.color;
-            bear.save(function (err) {
+            bear.save(function(err) {
                 if (err)
                     res.send(err);
 
@@ -69,10 +93,10 @@ router.route('/bears/:bear_id')
             });
         });
     })
-    .delete(function (req, res) {
+    .delete(function(req, res) {
         Bear.remove({
             _id: req.params.bear_id
-        }, function (err, bear) {
+        }, function(err, bear) {
             if (err)
                 res.send(err);
 
